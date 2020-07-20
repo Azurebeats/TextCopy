55634d4348c7b2be2cfbd26d7a83e9ac94dffdfa
Simon Cropp <simon.cropp@gmail.com>
2020-07-20 21:34:11 +1000

    .

---
+++ src/build-macos.ps1
@@ -0,0 +1,5 @@
+msbuild.exe src/TextCopy.sln /t:restore /p:Configuration=Release -verbosity:quiet
+
+msbuild.exe src/TextCopy.sln /t:build /p:Configuration=Release -verbosity:quiet
+
+dotnet test src --configuration Release --no-build --no-restore
